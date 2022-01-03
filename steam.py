4f6bb85311cce86c68c3de95c68f345002a09b43
Jeet Undhad <jrundhad@gmail.com>
2022-01-03 12:18:53 -0700

    got reviews

--- steam.py
+++ steam.py
@@ -1,30 +1,30 @@
-
 # importing the requests library
 import requests
+from datetime import datetime
   
 # api-endpoint
 URL = "http://store.steampowered.com/appreviews/230410?json=1?"
-  
+cursor = "*"
 
   
 # defining a params dict for the parameters to be sent to the API
-PARAMS = {
-    'filter':"recent",
-    'language': "english",
-    'review_type' :"all",
-    'purchase_type': "",
-    'num_per_page': "1"
-    }
-  
-# sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS)
-  
-# extracting data in json format
-data = r.json()
-reviews = data["reviews"]
-  
-# extracting latitude, longitude and formatted address 
-# of the first matching location
-for review in reviews:
-    print(review['review'])
+while(cursor != None):
+    PARAMS = {
+        'filter':"updated",
+        'language': "english",
+        'review_type' :"all",
+        'cursor' : cursor,
+        'purchase_type': "",
+        'num_per_page': "100"
+        }
+    
+    # sending get request and saving the response as response object
+    r = requests.get(url = URL, params = PARAMS)
+    
+    # extracting data in json format
+    data = r.json()
+    reviews = data["reviews"]
+    cursor = data["cursor"]
+    for review in reviews:
+        print(datetime.fromtimestamp(review['timestamp_updated']))
   
